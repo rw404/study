3956f4551d32881c3a330f4202e2761794c037e6
Ubuntu <ubuntu@ip-172-31-39-251.us-east-2.compute.internal>
2020-05-01 12:40:21 +0000

    f3 is correct

--- f3.asm
+++ f3.asm
@@ -1,11 +1,8 @@
-extern printf
 section .data
-  a dq -1.0
-  del dq 2.0
-  helper dq 1.0
-  flag dq 0.0
-section .rodata
-  LC0 db "%f", 0
+  a dq -1.0            ;to neg elements
+  del dq 2.0           ;using in loop to mult [helper]
+  helper dq 1.0        ;2^[-x]; [-x] - whole part of -x
+  flag dq 0.0          ;to cmp -x with zero
 section .text
 global f3
 f3:
@@ -13,133 +10,73 @@ f3:
   mov ebp, esp
   sub esp, 16
   and esp, 0xfffffff0
-  mov eax, 0
+  mov eax, 0           ;flag of x's sign
   finit
 
-  fld qword[ebp+8];ebp+8 x
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
+  fld qword[ebp+8]     ;x
+  
+  fmul qword[a]        ;-x
+  fldl2e               ;log2 e
+  fmulp                ;log2 e^-x; pop log2 e
+ 
+  fstp qword[esp]      ;result in [esp]; stack is empty
+  
+ 
+  fld qword[flag]      ;ST1
+  fld qword[esp]       ;ST0
+  fucomip st1          ;ST0 vs ST1; pop [esp]
 
-  fmul qword[a];-x
-  fldl2e;log2 e
-  fmulp; log2 e^-x
-  ;fld1
-  ;fsubp; qword[helper]
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
-
-  fstp qword[esp]
-  ;mov dword[esp], LC0
-  ;call printf
-  fld qword[flag];ST1
-  fld qword[esp];ST0
-  fucomip st1;st0 vs st1
-
-  jnc .dontchangeflag;if st0>=0
-  fld qword[helper]
-  ;fst qword[esp+4]
-  fstp qword[flag]
-  ;mov dword[esp], LC0
-  ;call printf
-  mov eax, 1
+  jnc .dontchangeflag  ;if st0>=0 -> don't change
+ 
+  mov eax, 1           ;if st0<0 flag = 1
   
 .dontchangeflag:
-  ;fstp
-  fld qword[esp]
-  fabs
-  ;fld qword[flag]
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
+  fstp                 ;stack is empty  
+  fld qword[esp]       
+  fabs                 ;|log2 e^-x|
 
 .L1:
   
-  fstp qword[esp]
-  fld1; ST1
-  fld qword[esp]; ST0
+  fstp qword[esp]      ;result in [esp]
+  fld1                 ;ST1
+  fld qword[esp]       ;ST0
 
-  fucomip st1;ST0 vs ST1
-  fstp
+  fucomip st1          ;ST0 vs ST1; pop [esp]
+  fstp                 ;pop 1 from stack; stack is empty
   fld qword[esp]
-  ;fld qword[esp]
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
 
-
-  jbe .result
+  jbe .result          ;if [esp] <= 1 jump to result
   
-  fld qword[helper]
-  fmul qword[del]
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
-
+  fld qword[helper]    ;else
+  fmul qword[del]      ;[helper] = [helper] * 2
 
   fstp qword[helper]
   
-  fld1
+  fld1                 ;ST1  = [esp]-1
 
-  fsubp
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
+  fsubp 
 
   jmp .L1
 
 .result:
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
-  ;fld qword[flag]
-  ;fld1;ST1
-  ;fld qword[flag]
-  ;fstp qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
-  cmp eax, 1
-  jb .end
+  cmp eax, 1 
+  jb .end              ;if log2 e^-x >= 0 jump to .end
   
-;  fucomip st1
-;  fstp
-;  jb .hthanzero
-;  fstp
-
-  fld1
+  fld1                 ;else [helper] = 1/[helper]
   fld qword[helper]
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
-
-;Zdes' shpion!!!!!
-
-  fdivp
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
-
-
+  fdivp 
   fstp qword[helper]
-  fstp qword[esp]
-  fld qword[esp]
-  fmul qword[a]
-;  jmp .end
 
-;.hthanzero:
-  ;fstp 
+  fmul qword[a]        ;ST0 = [esp]* -1; result after loop
+ 
 .end:
-  ;fst qword[esp+4]
-  ;mov dword[esp], LC0
-  ;call printf
 
-  f2xm1
+  f2xm1                ;-1 <= ST0 <= 1; ST0 = 2^ST0 - 1
   fld1
 
-  faddp
+  faddp                ;ST0++; in ST0 2^result_after_loop
   
-  fmul qword[helper]
+  fmul qword[helper]   ;ST0 = [helper] * ST0 = e^-x
 
   leave 
   ret
